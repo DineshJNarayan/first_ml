f9c8326376593d5d66ce1e04e5b14d4a4a095a03
Dinesh Narayan <dinesh@sourcepoint.com>
2022-01-04 00:23:00 +0100

    simple ai example - 04012022:0022

--- iris.py
+++ iris.py
@@ -48,3 +48,15 @@ print(dataset.shape)
 print(dataset.head(20))
 # descriptions
 print(dataset.describe())
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+
+# histograms
+dataset.hist()
+pyplot.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
