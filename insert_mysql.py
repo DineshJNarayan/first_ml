dea6bad42275e5eff9f4a834bd448114ed646b49
Dinesh Narayan <dinesh@sourcepoint.com>
2022-01-04 20:35:47 +0100

    insert into mysql db - 04012022:2035

--- insert_mysql.py
+++ insert_mysql.py
@@ -3,15 +3,6 @@ import pandas as pd
 # Load libraries
 from pandas import read_csv
 
-## mydb = mysql.connector.connect(
-##  host="127.0.0.1",
-##  user="root",
-##  password="Vmcp2020Gain",
-##  database="MachineLearning"
-##)
-
-##mycursor = mydb.cursor()
-
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
 dataset = read_csv(url, names=names)
@@ -35,11 +26,4 @@ def insert_variables_into_table(sepal_length, sepal_width, petal_length, petal_w
             print("MySQL connection is closed")
 
 for index, row in df.iterrows():
-    ## sql = "INSERT INTO iris_ml (sepal_length, sepal_width, petal_length, petal_width, class) VALUES (%s, %s, %s, %s, %s)" 
-    ## val = (row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
-    ## mycursor.execute(sql, val) 
-    ## mydb.commit()
-    ## print(row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
     insert_variables_into_table(row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class'])
-
-## print(mycursor.rowcount, "record inserted.")
