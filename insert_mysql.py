e213450d3047fff762db9ccbb180236a45d9719c
Dinesh Narayan <dinesh@sourcepoint.com>
2022-01-04 21:14:58 +0100

    insert into mysql db - 04012022:2114

--- insert_mysql.py
+++ insert_mysql.py
@@ -12,10 +12,10 @@ def insert_variables_into_table(sepal_length, sepal_width, petal_length, petal_w
     try:
         mydb = mysql.connector.connect(host="127.0.0.1", user="root", password="Vmcp2020Gain", database="MachineLearning")
         mycursor = mydb.cursor()
-        sql = "INSERT INTO iris_ml (sepal_length, sepal_width, petal_length, petal_width, class) VALUES (%s, %s, %s, %s, %s)" 
-        val = (row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
-        mycursor.execute(sql, val)
-        mydb.commit()
+        ## sql = "INSERT INTO iris_ml (sepal_length, sepal_width, petal_length, petal_width, class) VALUES (%s, %s, %s, %s, %s)" 
+        ## val = (row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
+        ## mycursor.execute(sql, val)
+        ## mydb.commit()
         print(sepal_length, sepal_width, petal_length, petal_width, iris_class) 
     except mysql.connector.Error as error:
         print("Failed to insert into MySQL table {}".format(error))
