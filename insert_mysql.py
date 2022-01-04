f349217e60fa4446fe640739da32e49f07c31608
Dinesh Narayan <dinesh@sourcepoint.com>
2022-01-04 21:12:00 +0100

    insert into mysql db - 04012022:2111

--- insert_mysql.py
+++ insert_mysql.py
@@ -12,9 +12,14 @@ dataset = read_csv(url, names=names)
 df = pd.DataFrame(dataset)
 
 for index, row in df.iterrows():
-    sql = "INSERT INTO iris_ml (sepal_length, sepal_width, petal_length, petal_width, class) VALUES (%s, %s, %s, %s, %s)" 
-    val = (row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
-    mycursor.execute(sql, val) 
-    mydb.commit()
+    ## sql = "INSERT INTO iris_ml (sepal_length, sepal_width, petal_length, petal_width, class) VALUES (%s, %s, %s, %s, %s)" 
+    ## val = (row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
+    ## mycursor.execute(sql, val) 
+    ## mydb.commit()
     print(row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
     print(mycursor.rowcount, "record inserted.")
+
+if mydb.is_connected():
+    mycursor.close()
+    mydb.close()
+    print("MySQL connection is closed")
