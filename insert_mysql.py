d6837637af8744c8381be9d56a051b3824b74f07
Dinesh Narayan <dinesh@sourcepoint.com>
2022-01-04 20:14:04 +0100

    insert into mysql db - 04012022:2013

--- insert_mysql.py
+++ insert_mysql.py
@@ -1,24 +1,45 @@
 import mysql.connector
+import pandas as pd
 # Load libraries
 from pandas import read_csv
 
-mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="root",
-  password="Vmcp2020Gain",
-  database="MachineLearning"
-)
+## mydb = mysql.connector.connect(
+##  host="127.0.0.1",
+##  user="root",
+##  password="Vmcp2020Gain",
+##  database="MachineLearning"
+##)
 
-mycursor = mydb.cursor()
+##mycursor = mydb.cursor()
 
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
 dataset = read_csv(url, names=names)
+df = pd.DataFrame(dataset)
 
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s, %s, %s, %s)"
-val = ("John", "Highway 21")
-mycursor.execute(sql, val)
+def insert_variables_into_table(sepal_length, sepal_width, petal_length, petal_width, iris_class):
+    try:
+        mydb = mysql.connector.connect(host="127.0.0.1", user="root", password="Vmcp2020Gain", database="MachineLearning")
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO iris_ml (sepal_length, sepal_width, petal_length, petal_width, class) VALUES (%s, %s, %s, %s, %s)" 
+        val = (row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(sepal_length, sepal_width, petal_length, petal_width, iris_class) 
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+    finally:
+        if mydb.is_connected():
+            mycursor.close()
+            mydb.close()
+            print("MySQL connection is closed")
 
-mydb.commit()
+for index, row in df.iterrows():
+    ## sql = "INSERT INTO iris_ml (sepal_length, sepal_width, petal_length, petal_width, class) VALUES (%s, %s, %s, %s, %s)" 
+    ## val = (row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
+    ## mycursor.execute(sql, val) 
+    ## mydb.commit()
+    ## print(row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class']) 
+    insert_variables_into_table(row['sepal_length'], row['sepal_width'], row['petal_length'], row['petal_width'], row['class'])
 
-print(mycursor.rowcount, "record inserted.")
+## print(mycursor.rowcount, "record inserted.")
