504235ca4ba41a5ef8affbe74aa6d277741ff91c
Dinesh Narayan <dinesh@sourcepoint.com>
2022-01-04 19:23:29 +0100

    insert into mysql db - 04012022:1923

--- insert_mysql.py
+++ insert_mysql.py
@@ -1,15 +1,21 @@
 import mysql.connector
+# Load libraries
+from pandas import read_csv
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
+  host="127.0.0.1",
+  user="root",
+  password="Vmcp2020Gain",
+  database="MachineLearning"
 )
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+dataset = read_csv(url, names=names)
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s, %s, %s, %s)"
 val = ("John", "Highway 21")
 mycursor.execute(sql, val)
 
