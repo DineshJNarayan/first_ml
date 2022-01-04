1ee0f74fcd34e1278e4eb08d29c669749f3e68b4
Dinesh Narayan <dinesh@sourcepoint.com>
2022-01-05 00:00:23 +0100

    simple ai example - 05012022:0000

--- iris.py
+++ iris.py
@@ -1,28 +1,14 @@
-# Check the versions of libraries
-
-# Python version
+# python version
 import sys
-print('Python: {}'.format(sys.version))
-# scipy
 import scipy
-print('scipy: {}'.format(scipy.__version__))
-# numpy
 import numpy
-print('numpy: {}'.format(numpy.__version__))
-# matplotlib
 import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
-# pandas
 import pandas
-print('pandas: {}'.format(pandas.__version__))
-# scikit-learn
 import sklearn
-print('sklearn: {}'.format(sklearn.__version__))
-
 import mysql.connector
 
 # Load libraries
-from pandas import read_csv
+## from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
